63ad4e8223e4fa61f8fdd9637e2b8b432c376d0d
PoglianiMattia <mattia.pogliani@gmail.com>
2020-03-27 11:39:18 +0100

    add classes and main

--- Dialogo_3/src/dialogo_3/Dialogo_3.java
+++ Dialogo_3/src/dialogo_3/Dialogo_3.java
@@ -15,7 +15,14 @@ public class Dialogo_3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        InteractFile file = new InteractFile("copione.txt");
+        
+        Thread actor1 = new Thread(new Actor("actor1", file));
+        Thread actor2 = new Thread(new Actor("actor2", file));
+        
+        actor1.start();        
+        actor2.start();
+        
     }
     
 }
